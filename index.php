e4b53ec4326231a8255112b1db0563257845c58a
Guilherme Costa <guilhermect1999@gmail.com>
2019-06-03 22:52:14 -0300

    Commiting first files

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<?php
+	include'DB.php';
+?>
+<html>
+  <head>
+     <title></title>
+     <meta charset="utf-8" />
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+     <script src='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js'></script>
+     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+
+  
+    <script>
+    	$(function(){
+
+    	})
+    </script>
+  </head>
+
+  <body>
+    	
+   
+  </body>
+</html>
